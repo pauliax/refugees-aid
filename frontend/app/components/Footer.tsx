1c36823a16c91031a60a498fa3dfa15e3bb1a24e
Paulius <pauliax6@gmail.com>
2025-01-06 21:32:26 +0200

    Add contract link to the footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import {Copyright} from "lucide-react";
+import {CONTRACT_ADDRESS} from '@/types/contract';
+import {formatAddress, getAddressLink} from "@/utils/utility-functions";
 
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -10,6 +12,9 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto flex items-center justify-center text-retro-brown font-mono">
         <Copyright className="w-4 h-4 mr-2"/>
         {year} Refugee Aid
+      </div>
+      <div className="mx-auto flex items-center justify-center text-retro-brown font-mono">
+        Contract&nbsp;<a href={getAddressLink(CONTRACT_ADDRESS)} target="_blank">{formatAddress(CONTRACT_ADDRESS)}</a>
       </div>
     </footer>
   )
